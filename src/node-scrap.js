4440765ab35e43d933aba20dc11e8dc174e28eb3
Nicolás Gonzálvez <n.gonzalvez@outlook.com>
2022-08-10 01:09:19 -0300

    Rename library

---
+++ src/node-scrap.js
@@ -0,0 +1,72 @@
+const http = require("http.min");
+const cheerio = require("cheerio");
+const Logger = require("./Logger");
+
+class Scraper {
+  constructor() {
+    this.halt = false;
+    this.params = this.getInitialParams();
+    this.firstTime = true;
+  }
+
+  setLogLevel(lvl) {
+    Logger.level = lvl;
+  }
+
+  getInitialParams() {
+    return {};
+  }
+
+  getNextUrl() {
+    return "";
+  }
+
+  async fetchDocument(url) {
+    const response = await http(url);
+    Logger.log(response.data, 2);
+    return cheerio.load(response.data);
+  }
+
+  scrap($) {
+    return {};
+  }
+
+  process(data) {}
+  afterEach() {}
+
+  stop() {
+    Logger.log("Stopping scrapper");
+    this.halt = true;
+  }
+
+  async step() {
+    let url = this.getNextUrl();
+    let doc;
+    let data;
+
+    Logger.log(`Fetching ${url}`);
+    doc = await this.fetchDocument(url);
+
+    Logger.log("Scraping document");
+    data = this.scrap(doc, url);
+
+    Logger.log("Processing data");
+    Logger.log(data, 2);
+    this.process(data);
+
+    this.afterEach();
+    this.firstTime = false;
+
+    if (!this.halt) {
+      this.step();
+    }
+  }
+
+  run() {
+    this.halt = false;
+    this.step().catch((err) => console.log(err));
+  }
+}
+
+module.exports = Scraper;
+module.exports.Logger = Logger;
